a23834ecfb741799f02fba853e11a9042e05b7f4
nishat <nishatroyhana011@gmail.com>
2022-09-03 07:30:56 +0600

    fetch news details

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const fetchCategories = async() =>{
     try{
         const res = await fetch('https://openapi.programming-hero.com/api/news/categories') ;
         const data = await res.json();
-        // console.log(data.data.news_category)
         return data.data.news_category;
     }
     catch(error){
-        //console.log(error);
+        console.log(error);
+        alert('Data not found');
     }
 }
 
@@ -18,8 +18,9 @@ const showCategories= async () =>{
         const categoryBtn = document.createElement('button');
         categoryBtn.classList.add("text-black", "p-2","text-gray-700", "hover:bg-indigo-50", "rounded-lg");
         categoryBtn.innerText = category_name;
-        categoryBtn.addEventListener('click', function(){
-            fetchDetails(category_id);
+        categoryBtn.addEventListener('click', function (){
+            const  fetchedDetails = fetchDetails(category_id);
+            showNews(fetchedDetails);
          });
         categories.appendChild(categoryBtn);
     })
@@ -29,12 +30,20 @@ const fetchDetails = async (id) => {
     try{
        const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${id}`) ;
        const data = await res.json();
-       console.log(data.data)
        return data.data;
    }
    catch(error){
        console.log(error);
+       alert('Data not found');
    }
 } 
 
+const showNews = async (details) =>{
+    const newsSec = document.getElementById('news-section');
+    const detailNews = await details;
+    detailNews.forEach(news => {
+        console.log(news)
+        const {title, details, total_view, thumbnail_url } = news;
+    })
+} 
 showCategories();
